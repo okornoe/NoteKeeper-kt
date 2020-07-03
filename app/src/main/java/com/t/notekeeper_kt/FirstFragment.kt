e0636798881d2e10d11efb95f4f2dbb7dc652dd4
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-07-03 14:58:20 +0000

    populated the list of courses into spinner

--- app/src/main/java/com/t/notekeeper_kt/FirstFragment.kt
+++ app/src/main/java/com/t/notekeeper_kt/FirstFragment.kt
@@ -1,12 +1,12 @@
 package com.t.notekeeper_kt
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -23,5 +23,17 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val dm = DataManager()
+
+        val adapterCourses = activity?.let {
+            ArrayAdapter<CourseInfo>(
+                it,
+                android.R.layout.simple_spinner_dropdown_item, dm.courses.values.toList()
+            )
+        }
+
+        adapterCourses?.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerCourses.adapter = adapterCourses
     }
 }
