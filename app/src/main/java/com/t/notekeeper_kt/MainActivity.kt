73960f9774ec643d9ad6daefbed121f83dc8bc45
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-07-03 14:56:17 +0000

    removed code from main activity to FirstFragment.kt

--- app/src/main/java/com/t/notekeeper_kt/MainActivity.kt
+++ app/src/main/java/com/t/notekeeper_kt/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.t.notekeeper_kt
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import kotlinx.android.synthetic.main.fragment_first.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,16 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
-
-        val dm = DataManager()
-        val adapterCourses = ArrayAdapter<CourseInfo>(this,
-            android.R.layout.simple_spinner_item, dm.courses.values.toList())
-
-        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-        //spinner.adapter = adapterCourses
-        spinnerCourses.adapter = adapterCourses
-
 
     }
 
