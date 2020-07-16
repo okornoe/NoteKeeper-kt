bdd3e9188e761a41be81bb736dbb2bab4fe913ee
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-07-16 11:26:08 +0000

    change setContentView from fragment_note_list to activity_main

--- app/src/main/java/com/t/notekeeper_kt/MainActivity.kt
+++ app/src/main/java/com/t/notekeeper_kt/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_note_list)
+        setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
     }
